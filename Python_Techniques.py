d054ddf57b71c00686f8a940c81c556a9bf9a1ae
RolandMarton <roland111187@gmail.com>
2021-10-27 04:57:04 +0200

    Fix

--- Python_Techniques.py
+++ Python_Techniques.py
@@ -773,7 +773,7 @@ print(create_dictionary())
 dictionary = {'t': 1, 'h': 2, 'e': 3, 'q': 1, 'u': 2, 'i': 1, 'c': 1, 'k': 1, 'b': 1, 'r': 2, 'o': 4, 'w': 1, 'n': 1, 'f': 1, 'x': 1, 'j': 1, 'm': 1, 'p': 1, 's': 1, 'v': 1, 'l': 1,  'a': 1, 'z': 1, 'y': 1, 'd': 1, 'g': 1}
 
 def sort_keys(elements):
-    keys = list(elements.keys())
+    keys = elements.keys()
     return sorted(keys)
 
 keys = sort_keys(dictionary)
